99a06b3284e3320190e6242d505763a9be151cff
KKHoon210417 <zozo1591@naver.com>
2021-09-25 01:31:08 +0900

    중복 기능 삭제, 페이지 이동 기능 추가 #9

--- app.py
+++ app.py
@@ -17,36 +17,6 @@ def recipe_list():
     recipe_list = list(db.recipe_basic.find({},{'_id':False}))
     return jsonify({'recipe_list':recipe_list})
 
-# 레시피 상세 보기
-@app.route('/recipe/code', methods=['GET'])
-def detail_recipe():
-    recipe_id = int(request.args.get('recipe-id'))
-    recipe_number = list(db.recipe_number.find({'RECIPE_ID':recipe_id},{'_id':False}))
-    recipe = db.recipe_basic.find_one({'RECIPE_ID':recipe_id}, {"_id": False})
-    return jsonify({'recipe_process':recipe_number, 'recipe':recipe})
-
-# # 댓글 DB저장
-# @app.route('/recipe/comment', methods=['POST'])
-# def recipe_comment():
-#     user_name = request.form['userName']
-#     user_comment = request.form['userComment']
-#     recipe_id = request.form['recipeId']
-#     doc = {
-#         'RECIPE_ID' : recipe_id,
-#         'USER_NM' : user_name,
-#         'USER_COMT' : user_comment
-#     }
-#     db.comment.insert_one(doc)
-#
-#     return jsonify({'msg':'성공'})
-#
-# # 댓글 보기
-# @app.route('/recipe/comment', methods=['GET'])
-# def comment_list():
-#     recipe_id = request.args.get('recipe-id')
-#     recipe_comment = list(db.comment.find({'RECIPE_ID':recipe_id},{'_id':False}))
-#     print(recipe_comment)
-#     return jsonify({'recipe_comment':recipe_comment})
 
 # 레시피 상세정보 API
 @app.route('/recipe/detail', methods=['GET'])
@@ -65,22 +35,6 @@ def get_recipe_detail():
 
     return jsonify({"info":info, "detail": detail})
 
-# 댓글 목록 API
-@app.route('/recipe/comment', methods=['GET'])
-def get_comments():
-    recipe_id = int(request.args.get("recipe_id"))
-    print(recipe_id)
-    comments = list(db.comment.find({"RECIPE_ID": recipe_id}, {"_id": False}))
-    print(f'comments = {comments}')
-
-    return jsonify(comments)
-
-
-# 댓글 작성 API
-@app.route('/recipe/comment', methods=['POST'])
-def save_comment():
-    recipe_id = int(request.form["recipe_id"])
-    text = request.form["text"]
 
     # [업로드 이미지 처리]
     # 클라이언트가 업로드한 파일을 서버에 저장
@@ -118,6 +72,8 @@ def save_comment():
     db.comment.insert_one(doc)
 
     return jsonify({'result': 'success'})
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
