0dfc3438990730291aa4454eaa8afdc9845a18c0
hellonayeon <hellonayeon@icloud.com>
2021-09-24 21:56:58 +0900

    댓글 저장 기능 추가, 이미지 업로드 기능 추가 #7

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbrecipe
 
+from datetime import datetime
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -15,7 +18,7 @@ def get_recipe_detail():
     recipe_id = int(request.args.get("recipe_id"))
 
     # 레시피 정보
-    projection = {"RECIPE_NM_KO": True, "SUMRY": True, "NATION_NM": True,
+    projection = {"RECIPE_ID": True, "RECIPE_NM_KO": True, "SUMRY": True, "NATION_NM": True,
                   "COOKING_TIME": True, "QNT": True, "IMG_URL": True, "_id": False}
     info = db.recipe_basic.find_one({"RECIPE_ID": recipe_id}, projection)
     print(info)
@@ -25,6 +28,60 @@ def get_recipe_detail():
     detail = list(db.recipe_number.find({"RECIPE_ID": recipe_id}, projection))
 
     return jsonify({"info":info, "detail": detail})
+
+# 댓글 목록 API
+@app.route('/recipe/comment', methods=['GET'])
+def get_comments():
+    recipe_id = int(request.args.get("recipe_id"))
+    print(recipe_id)
+    comments = list(db.comment.find({"RECIPE_ID": recipe_id}, {"_id": False}))
+    print(f'comments = {comments}')
+
+    return jsonify(comments)
+
+
+# 댓글 작성 API
+@app.route('/recipe/comment', methods=['POST'])
+def save_comment():
+    recipe_id = int(request.form["recipe_id"])
+    text = request.form["text"]
+
+    # [업로드 이미지 처리]
+    # 클라이언트가 업로드한 파일을 서버에 저장
+    fname = ""
+    extension = ""
+    today = today = datetime.now()  # 현재 시각 가져오기
+    if len(request.files) != 0:
+        file = request.files["img_src"]
+
+        # 이미지 확장자
+        # 가장 마지막 문자열 가져오기 [-1]
+        ### 아이폰 heic 확장자 이미지 예외처리 필요
+        extension = file.filename.split('.')[-1]
+
+        today = datetime.now()  # 현재 시각 가져오기
+        time = today.strftime('%Y-%m-%d-%H-%M-%S')
+        fname = f'file-{time}.{extension}'
+
+        save_to = f'static/images/{fname}'  # python f-string
+        file.save(save_to)  # 날짜 기반 새로운 파일 이름 생성 후 프로젝트 static/images/ 폴더에 저장
+
+        print(recipe_id, text, file)
+
+    # 업데이트 날짜 표시
+    date = today.strftime('%Y.%m.%d')
+
+    # [DB 처리]
+    doc = {
+        'RECIPE_ID': recipe_id,
+        'TEXT': text,
+        'IMG_SRC': fname,
+        'DATE': date
+    }
+
+    db.comment.insert_one(doc)
+
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
